825006b55ef266d50e003af3d20ac3e4dbbb2b2c
Punish Midha <punishmidha4@gmail.com>
2024-07-25 19:46:33 +0530

    Initital Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// const links = document.querySelectorAll('.nav-link');
+    
+// if (links.length) {
+//   links.forEach((link) => {
+//     link.addEventListener('click', (e) => {
+//       links.forEach((link) => {
+//           link.classList.remove('active');
+//       });
+//       e.preventDefault();
+//       link.classList.add('active');
+//     });
+//   });
+// }
+
+// Initialization for ES Users
+import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple });
